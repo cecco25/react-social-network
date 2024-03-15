0d04f0e9c902fcd37f8b9840ca7692463a5c811d
CeccoBolt <cecchinimichael.code@gmail.com>
2024-03-15 15:26:19 +0100

    Add props to Post component

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.post}>
-        <Post />
+        <Post username={"Utente Prova"} imagePath={"https://images.unsplash.com/photo-1710475512344-853809054d97?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} text={"Lorem Ipsum"} />
       </div>
       <div className={styles.nav}>
         <Navbar />
--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,16 +1,22 @@
+"use client"
+
 import './post-style.css';
 import Image from 'next/image';
 
-export default function Post() {
+export default function Post({ username, imagePath, text }) {
     return (
         <div className='post-bg'>
             <div className='user-info'>
-                <Image src={"/assets/user-pic.jpeg"} width={30} height={30} alt='user pic' />
-                <span>Nome Utente</span>
+                <Image src={imagePath} width={40} height={40} alt='user pic'
+                    onError={(e) => {
+                        e.currentTarget.src = "/assets/user-pic.jpeg";
+                        e.currentTarget.onerror = null;
+                    }} />
+                <span>{username}</span>
             </div>
             <div className='post-text'>
                 <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut tempora sint ad dicta hic, corporis atque. Eius quis blanditiis voluptates, ipsam nesciunt in unde nihil ratione nostrum, facere nulla labore.
+                    {text}
                 </p>
             </div>
             <div className='post-actions'>
