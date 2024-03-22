b20d2bf6ce3fe4fa7f7f6d727269aff3827747dc
CeccoBolt <cecchinimichael.code@gmail.com>
2024-03-22 14:39:05 +0100

    Add Navbar to layout

--- src/app/notifications/page.js
+++ src/app/notifications/page.js
@@ -6,7 +6,7 @@ import Navbar from "@/components/Navbar/Navbar"
 export default function Page() {
     return (
         <main className="main">
-            <Navbar />
+
         </main>
     )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -43,9 +43,6 @@ export default function Home() {
         ))}
 
       </div>
-
-      <Navbar />
-
     </main>
   );
 }
